7c17f0d046c715e666f881abe6e33f859f60e143
Salman Khan <salmantechhub336@gmail.com>
2026-02-27 01:23:59 +0500

    added tabs chat , calls , status

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,10 +10,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 4, 
+      child: Scaffold(
      appBar: AppBar(
+      backgroundColor: Colors.teal,
       title: Text('Whatsapp'),
+        bottom: TabBar(
+          tabs: [
+            Text('Chat'),
+            Text('Status'),
+            Text('Calls'),
+            Icon(Icons.camera_alt_rounded),
+          ]
+          ),
      ),
+     body: TabBarView(
+      children:[
+        Text('Chat'),
+         Text('Status'),
+          Text('Calls'),
+          Text('Camera'),
+      ]
+      ),
+    ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     title: 'flutter Demo',
     theme: ThemeData(
-      primarySwatch: Colors.blue
+      primarySwatch: Colors.teal,
     ),
     home: HomeScreen(),
     );
